b8b2db137ab89f1cd4f2000b6c5c70cf7c0c4046
FetterDev <60770700+FetterDev@users.noreply.github.com>
2020-03-16 21:00:03 +0300

    Add files via upload

--- finaljs/finaljs.js
+++ finaljs/finaljs.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 (function() {
     document.getElementById('newToDo').addEventListener('keydown', function(e) {
    if (e.keyCode === 13) {
@@ -12,41 +7,35 @@ let valueNewTask = function(){
    }
    let a = valueNewTask();
    let inputValue = a.value ;
-   
-    let createNewTaskElement = function(inpVl){
-        let listElmement = document.createElement('li');
-        let checkBox = document.createElement("input");
-        let spanElement = document.createElement("span");
-        let imgButElm = document.createElement("img");
-        imgButElm.src = 'img/delete.gif'
-        listElmement.classList.add("task");
-        imgButElm.classList.add("delete");
-        spanElement.innerText =  inpVl;
-        checkBox.type="checkbox";
-        listElmement.appendChild(checkBox);
-        listElmement.appendChild(spanElement);
-        listElmement.appendChild(imgButElm);
-        return (listElmement);
-    }
-    const item = createNewTaskElement(inputValue);
-   document.getElementById("ListUL").appendChild(item);
+   if (!inputValue=="")  {
+
+       let createNewTaskElement = function(inpVl){
+           let listElmement = document.createElement('li');
+           let checkBox = document.createElement("input");
+           let spanElement = document.createElement("span");
+           let imgButElm = document.createElement("img");
+           imgButElm.src = 'img/delete.gif'
+           listElmement.classList.add("task");
+           imgButElm.classList.add("delete");
+           spanElement.innerText =  inpVl;
+           checkBox.type="checkbox";
+           listElmement.appendChild(checkBox);
+           listElmement.appendChild(spanElement);
+           listElmement.appendChild(imgButElm);
+           return (listElmement);
+       }
+      const item = createNewTaskElement(inputValue);
+      document.getElementById("ListUL").appendChild(item);
+      document.getElementById('newToDo').value = "";
+   }
    }
     });
   })();
 
 
+let  delListItem=function(){
 
-
-
-var addTask=function(){
-	console.log("Add Task...");
-	//Create a new list item with the text from the #new-task:
-	var listItem=createNewTaskElement(inputValue);
-
-	//Append listItem to incompleteTaskHolder
-	incompleteTaskHolder.appendChild(listItem);
-	bindTaskEvents(listItem, taskCompleted);
-
-	taskInput.value="";
 
 }
+    let deleteButs = document.querySelectorAll("delete");
+    document.querySelectorAll("delete").addEventListener('click',delListItem)
